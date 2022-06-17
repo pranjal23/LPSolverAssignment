749f9af49a9f9a6426dd9c854f9a9f9906e9359b
Pranjal Swarup <pswarup@ntu.edu.sg>
2022-06-17 18:05:45 +0800

    Converting trade orders to DayTradeOrder WIP

--- src/main/java/solver/LPSolver.java
+++ src/main/java/solver/LPSolver.java
@@ -1,7 +1,25 @@
 package solver;
 
+import entity.DayTradeOrder;
+import entity.TraderOrders;
+
+import java.util.List;
+import java.util.Map;
+
 public class LPSolver {
 
     // Parse the traders orders and build the objectives and constraints.
     // Solve each objective sequentially.
+    List<TraderOrders> mTraderOrders;
+    Map<Integer, List<DayTradeOrder>> mDayOrders;
+
+    public LPSolver(List<TraderOrders> traderOrders){
+        this.mTraderOrders = traderOrders;
+    }
+
+    public void createDayTradeOrders(){
+            int currentDay = 1;
+            
+    }
+
 }
